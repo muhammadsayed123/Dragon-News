ca77b87a72dfd92bb332041eecdbdffb1499236f
muhammadsayed123 <muhammadsaye741@gmail.com>
2026-04-05 08:08:04 +0600

    add header,latestNews and navbar

---
+++ src/Components/LeatestNews.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import Marquee from "react-fast-marquee";
+
+const LatestNews = () => {
+  return (
+    <div className="flex items-center gap-3 bg-base-200 p-2">
+      <p className="text-base-100 bg-secondary px-3 py-1">Latest</p>
+        <p className="font-semibold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, unde.
+        </p>
+        {/* <Marquee></Marquee> */}
+    </div>
+  );
+};
+
+export default LatestNews;
--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Header from "../Components/Header";
+import LatestNews from "../Components/LeatestNews";
+import Navbar from "../Components/Navbar";
 
 const Root = () => {
   return (
     <div>
       <header>
         <Header></Header>
+        <section className="w-11/12 mx-auto my-3">
+            <LatestNews></LatestNews>
+        </section>
+        <nav className="w-11/12 mx-auto my-3">
+            <Navbar></Navbar>
+        </nav>
       </header>
       <main>
         <div className="left_nav"></div>
