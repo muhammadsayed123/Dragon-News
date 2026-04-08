c4ab34e449e267741d7800f84d52f3db61dbddd5
muhammadsayed123 <muhammadsaye741@gmail.com>
2026-04-08 21:34:30 +0600

    finish module 50

---
+++ src/Components/HomeLayout/FindUs.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+
+const FindUs = () => {
+    return (
+        <div>
+            <h1 className='font-bold mb-5'>Find Us On</h1>
+            <div className='join join-vertical w-full'>
+                <button className='btn join-item bg-base-100 justify-start'><FaFacebook></FaFacebook> Facebook</button>
+                <button className='btn join-item bg-base-100 justify-start'>< FaTwitter></FaTwitter> Twitter</button>
+                <button className='btn join-item bg-base-100 justify-start'><FaInstagram></FaInstagram> Instagram</button>
+            </div>
+        </div>
+    );
+};
+
+export default FindUs;
--- src/Page/CategoryNews.jsx
+++ src/Page/CategoryNews.jsx
@@ -30,10 +30,49 @@ const CategoryNews = () => {
 
     <div className="grid grid-cols-1 gap-5">
         {
-            categoryNews.map(news=> <NewsCard key={news.id} news={news}></NewsCard>)
+            categoryNews.map(news=> <NewsCard key={news} news={news}></NewsCard>)
         }
     </div>
   </div>;
 };
 
 export default CategoryNews;
+
+
+
+// import React from "react";
+// import { useLoaderData, useParams } from "react-router";
+// import NewsCard from "../Components/NewsCard";
+
+// const CategoryNews = () => {
+//   const data = useLoaderData();
+//   const { id } = useParams();
+
+//   // DERIVED STATE: Calculate the filtered news directly during render
+//   let categoryNews = [];
+
+//   if (id === "0") {
+//     categoryNews = data;
+//   } else if (id === "1") {
+//     categoryNews = data.filter((news) => news.others.is_today_pick === true);
+//   } else {
+//     // Note: Use == or Number(id) if your category_id is a number
+//     categoryNews = data.filter((news) => news.category_id == id);
+//   }
+
+//   return (
+//     <div>
+//       <h2 className="font-bold mb-5">
+//         Total <span className="text-secondary">{categoryNews.length}</span> news found
+//       </h2>
+
+//       <div className="grid grid-cols-1 gap-5">
+//         {categoryNews.map((news) => (
+//           <NewsCard key={news._id || news.id} news={news} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CategoryNews;
